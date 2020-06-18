ece1708b4a213ddb61f20004015485b97a44f1ca
Roman Shkabko <33148760+rshkabko@users.noreply.github.com>
2020-06-18 13:46:10 +0300

    Конвертация картинок в WebP

---
+++ webp.php
@@ -0,0 +1,89 @@
+<?php
+ini_set('max_execution_time', '0');
+ini_set("memory_limit",-1);
+
+var_dump(WEBP::init('.'));
+
+
+class WEBP {
+    public static $patterns = array(
+        '/.*\.jpg/',
+        '/.*\.jpeg/',
+        '/.*\.png/',
+        );
+    
+    public static function init($folder = '.')
+    {
+        //Для проверки
+        self::getWebPPath();
+        
+        $files = self::getFiles($folder);
+        $new_files = array();
+        
+        if(empty($files))
+            return false;
+        
+        foreach($files as $file)
+            if( is_file($file) && !file_exists($file . '.webp') ) {
+                $new_files[] = $file;
+                self::exec($file);
+            }
+        
+        return $new_files; 
+    }
+    
+    /**
+     * Возвращаем все файлы по предустановленным патерном
+     */  
+    public static function getFiles($folder = '.')
+    {
+        $files = array();
+        foreach(self::$patterns as $pattern)
+            foreach(self::rsearch($folder, $pattern) as $file)
+                $files[] = $file;
+                
+        return $files;
+    }
+    
+    /**
+     * Рекурсивный поиск файлов по патерну
+     */
+    public static function rsearch($folder, $pattern)
+    {
+        $dir = new RecursiveDirectoryIterator($folder);
+        $ite = new RecursiveIteratorIterator($dir);
+        $files = new RegexIterator($ite, $pattern, RegexIterator::GET_MATCH);
+        $fileList = array();
+        foreach($files as $file) {
+            $fileList = array_merge($fileList, $file);
+        }
+        return $fileList;
+    }
+    
+    /**
+    *   Выполняем команду
+    */
+    public static function exec( $file )
+    {
+        return exec( self::getWebPPath() . ' -quiet -q 90 "' . $file . '" -o "' . $file . '.webp"' );
+    }
+    
+    public static function getWebPPath()
+    {
+        if(isset($_SERVER['LOGNAME']))
+            $login = $_SERVER['LOGNAME'];
+        else {
+            preg_match('|\/var\/www\/(.*?)\/|sei', $_SERVER['DOCUMENT_ROOT'], $arr);
+            if(empty($arr['1']))
+                exit('Buy WebP');
+            $login = $arr['1'];
+        }
+            
+        $str = "/var/www/{$login}/data/cwebp";
+        
+        if(!file_exists($str))
+            exit('Buy WebP');
+            
+        return $str;
+    }
+}
